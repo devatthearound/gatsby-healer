c72ea7a3f8ff23a3e9a1e41d832f5432362d9890
Leegaeun <lgu@the-around.com>
2022-06-16 14:43:47 +0900

    add: createUser profile

--- src/middleware/auth.middleware.tsx
+++ src/middleware/auth.middleware.tsx
@@ -1,15 +1,22 @@
 import FBStoreService from "../service/FBStoreService";
+import FBStorageService from "../service/FBStorageService";
+import StorageMiddleware from "./storage.middleware";
 
 export default class AuthMiddleware {
     constructor(
-        private readonly firebaseStore = FBStoreService
+        private readonly firebaseStore = FBStoreService,
+        private readonly firebaseStorage = FBStorageService,
     ) { }
 
-    async createUser(userId: string, phoneNumber: string, name: string) {
+    async createUser(userId: string, phoneNumber: string, name: string, profile: File) {
+        const storeage = new StorageMiddleware();
+        const imageUrl = await storeage.uploadSigleImage(profile, 'User')
+
         const res = await this.firebaseStore.CreateStoreData("usersCollection", {
             phoneNumber: phoneNumber,
             userId: userId,
             name: name,
+            profile: imageUrl ?? imageUrl
         });
 
         if (!res) alert("유저 생성에 실패했습니다.");
