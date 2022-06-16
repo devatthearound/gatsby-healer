2b8254154b8ec6461b80363aa2dbaa3ddf1b424c
Leegaeun <lgu@the-around.com>
2022-06-16 16:04:29 +0900

    add: delete FB service

--- src/service/FBStorageService.tsx
+++ src/service/FBStorageService.tsx
@@ -1,4 +1,4 @@
-import { FirebaseStorage, getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { FirebaseStorage, getStorage, ref, uploadBytes, getDownloadURL, deleteObject } from 'firebase/storage';
 import firebase from './FBConfig';
 
 const firestorage: FirebaseStorage = getStorage(firebase, "gs://test-healer.appspot.com");
@@ -21,6 +21,19 @@ const StorageService = {
             }
         }
     },
+    DeleteImage: async (imageUrl: string): Promise<string> => {
+        const refStorage = ref(firestorage, imageUrl);
+        try {
+            await deleteObject(refStorage);
+            return "success"
+        } catch (err: unknown) {
+            if (err instanceof Error) {
+                throw new Error(err.message);
+            } else {
+                throw new Error("알 수 없는 오류입니다.");
+            }
+        }
+    }
 };
 
 export default StorageService;
