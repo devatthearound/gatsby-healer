e5b027687be460060104e0a33f31df32a3c7fe01
Leegaeun <lgu@the-around.com>
2022-06-17 15:22:05 +0900

    firestore service 유저 접근 권한 체크하기

--- src/service/FBStoreService.tsx
+++ src/service/FBStoreService.tsx
@@ -1,14 +1,18 @@
-import { getFirestore, doc, collection, updateDoc, setDoc, deleteDoc, getDoc, getDocs } from 'firebase/firestore';
+import { getFirestore, doc, collection, updateDoc, setDoc, deleteDoc, getDoc, getDocs, query, where } from 'firebase/firestore';
 import exceptionDTO from '../dto/exception.dto';
+import { useAuth } from '../hooks/SMSProvider';
 import firebase from './FBConfig';
 
 const firebaseStore = getFirestore(firebase);
+const { user } = useAuth();
 
 const FBStoreService = {
-    GetAllStoreData: async (store: string): Promise<any[] | exceptionDTO> => {
+    GetAllStoreData: async (store: string): Promise<any> => {
         try {
             let data: any[] = [];
-            const querySnapshot = await getDocs(collection(firebaseStore, store));
+            const q = query(collection(firebaseStore, store), where("userId", "==", user?.uid));
+            const querySnapshot = await getDocs(q);
+
             querySnapshot.forEach((q) => {
                 data.push({ ...q.data(), id: q.id })
             })
@@ -16,22 +20,15 @@ const FBStoreService = {
             return data;
         } catch (err: unknown) {
             if (err instanceof Error) {
-                console.log(err);
-                return {
-                    code: 500,
-                    message: err.message
-                };
+                throw new Error(err.message);
             } else {
-                return {
-                    code: 500,
-                    message: "알 수 없는 에러가 발생했습니다"
-                }
+                throw new Error("알 수 없는 오류입니다.");
             }
         }
     },
-    GetOneStoreData: async (store: string, id: string): Promise<any> => {
+    GetOneStoreData: async (store: string, storeId: string): Promise<any> => {
         try {
-            const querySnapshot = await getDoc(doc(firebaseStore, store, id));
+            const querySnapshot = await getDoc(doc(firebaseStore, store, storeId));
             const req = { ...querySnapshot.data(), id: querySnapshot.id }
             return req;
         } catch (err: unknown) {
@@ -42,10 +39,10 @@ const FBStoreService = {
             }
         }
     },
-    CreateStoreData: async (store: string, body: any): Promise<string> => {
+    CreateStoreData: async <T extends {}>(store: string, body: T): Promise<any> => {
         try {
             const newDocRef = await doc(collection(firebaseStore, store));
-            const docRef = await setDoc(newDocRef, { ...body });
+            const docRef = await setDoc(newDocRef, { ...body, userId: user?.uid });
 
             return newDocRef.id;
         } catch (err: unknown) {
@@ -56,45 +53,36 @@ const FBStoreService = {
             }
         }
     },
-    RemoveStoreData: async (id: string, store: string): Promise<string | exceptionDTO> => {
+    UpdateStoreData: async <T extends {}>(store: string, storeId: string, body: T): Promise<any> => {
         try {
-            const docRef = await deleteDoc(doc(firebaseStore, store, id));
-            return id
-        } catch (err: unknown) {
-            if (err instanceof Error) {
-                return {
-                    code: 500,
-                    message: err.message,
-                };
-            } else {
-                return {
-                    code: 500,
-                    message: "알 수 없는 오류입니다.",
-                }
-            }
-        }
-    },
-    UpdateStoreData: async (body: any, store: string): Promise<string | exceptionDTO> => {
-        try {
-            const newDocRef = doc(firebaseStore, store, body.id);
-            const docUpdate = await updateDoc(newDocRef, body);
-            return body.id
-        } catch (err: unknown) {
-            if (err instanceof Error) {
-                console.log(err);
-                return {
-                    code: 500,
-                    message: err.message,
-                };
-            } else {
-                return {
-                    code: 500,
-                    message: "알 수 없는 오류입니다.",
-                }
-            }
-        }
-    },
+            const newDocRef = doc(firebaseStore, store, storeId);
+            if (newDocRef.id != user?.uid) throw new Error("유저접근 권한이 없습니다.");
 
+            const docUpdate = await updateDoc(newDocRef, body);
+            return storeId
+        } catch (err: unknown) {
+            if (err instanceof Error) {
+                throw new Error(err.message);
+            } else {
+                throw new Error("알 수 없는 오류입니다.");
+            }
+        }
+    },
+    RemoveStoreData: async (store: string, storeId: string) => {
+        try {
+            const newDocRef = doc(firebaseStore, store, storeId);
+            if (newDocRef.id != user?.uid) throw new Error("유저접근 권한이 없습니다.");
+
+            const docRef = await deleteDoc(doc(firebaseStore, store, storeId));
+            return storeId
+        } catch (err: unknown) {
+            if (err instanceof Error) {
+                throw new Error(err.message);
+            } else {
+                throw new Error("알 수 없는 오류입니다.");
+            }
+        }
+    },
 };
 
 export default FBStoreService;
